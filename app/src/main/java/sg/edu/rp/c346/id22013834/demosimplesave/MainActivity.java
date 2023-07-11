8075340a856e7171d6c9cfa621783c3d919621f8
av4lyfo <22013834@myrp.edu.sg>
2023-07-11 14:11:03 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22013834/demosimplesave/MainActivity.java
@@ -0,0 +1,15 @@
+package sg.edu.rp.c346.id22013834.demosimplesave;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+}
